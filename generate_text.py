30babd0a21b67794cae4941f3b3f92d2214a9ab5
Ben Plaut <bplaut@rnn.ist.berkeley.edu>
2023-12-03 14:27:19 -0800

    Added support for sampling via -s and returning multiple sequences per prompt via -r

--- generate_text.py
+++ generate_text.py
@@ -10,6 +10,9 @@ def set_params():
     parser.add_argument('-n', '--max_new_tokens', type=int, help='Number of new tokens to generate on top of the prompt', default=10)
     parser.add_argument('-t', '--num_top_tokens', type=int, help='For each token, print out the top candidates considered by the model and their probabilities', default=0)
     parser.add_argument('-c', '--chat_mode', action="store_true", help='Whether to treat the prompt as a chat message and generate a chatbot response, vs just normal text auto-complete', default=False)
+    parser.add_argument('-s', '--do_sample', action="store_true", help='Should we sample from the probability distribution, or greedily pick the most likely token?', default=False)
+    parser.add_argument('-r', '--num_responses', type=int, help='Number of responses to generate per prompt. This argument is ignored for greedy decoding, since that only generates one answer.', default=1)
+    
     args = parser.parse_args()
     if args.model == 'Mistral-7B':
         model_name = 'mistralai/Mistral-7B-v0.1'
@@ -24,32 +27,32 @@ def set_params():
     else:
         raise Exception("Unrecognized model name. Try python generate_text -h")
     prompts = args.prompts.split('|')
-    return (model_name, prompts, args.max_new_tokens, args.num_top_tokens, args.chat_mode)
+    return (model_name, prompts, args.max_new_tokens, args.num_top_tokens, args.chat_mode, args.do_sample, args.num_responses)
 
 def prepare_for_chat(prompts, tokenizer):
     chats = [[{"role": "user", "content": p}] for p in prompts]
     return [tokenizer.apply_chat_template(c, tokenize=False, add_generation_prompt=True, return_tensors="pt") for c in chats]
 
     
-def generate(model_name, prompts, max_new_tokens, num_top_tokens, chat_mode):
+def generate(model_name, prompts, max_new_tokens, num_top_tokens, chat_mode, do_sample, num_responses):
     model = AutoModelForCausalLM.from_pretrained(model_name, device_map="auto", load_in_4bit=True)
     tokenizer = AutoTokenizer.from_pretrained(model_name, padding_side="left")
     tokenizer.pad_token = tokenizer.eos_token  # Most LLMs don't have a pad token by default
     if chat_mode:
         prompts = prepare_for_chat(prompts, tokenizer)
+    if not do_sample:
+        num_responses = 1
     model_inputs = tokenizer(prompts, return_tensors="pt", padding=True).to("cuda")
-    do_sample = False
-    num_return_sequences = 2 if do_sample else 1
 
-    output = model.generate(**model_inputs, max_new_tokens=max_new_tokens, do_sample=do_sample, output_scores=True, num_return_sequences=num_return_sequences, return_dict_in_generate=True, renormalize_logits=True)
+    output = model.generate(**model_inputs, max_new_tokens=max_new_tokens, do_sample=do_sample, output_scores=True, num_return_sequences=num_responses, return_dict_in_generate=True, renormalize_logits=True)
     text_outputs = tokenizer.batch_decode(output.sequences, skip_special_tokens=True)
 
 
     print('\n')
     for i in range(len(text_outputs)):
-        prompt_idx = i//num_return_sequences
+        prompt_idx = i//num_responses
         print('PROMPT %d: "%s"\n' % (prompt_idx+1, prompts[prompt_idx]))
-        print('OUTPUT: "%s"\n' % text_outputs[i])
+        print('OUTPUT %d: "%s"\n' % (i % num_responses + 1, text_outputs[i]))
         token_ids = output.sequences[i][len(model_inputs[prompt_idx]):]
 
         if num_top_tokens > 0:
@@ -68,6 +71,5 @@ def generate(model_name, prompts, max_new_tokens, num_top_tokens, chat_mode):
 def main():
     params = set_params()
     generate(*params)
-
         
 main()
