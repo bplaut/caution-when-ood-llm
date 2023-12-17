f2fa810f53116221c6439675319fc7ca71cfeceb
Ben Plaut <bplaut@rnn.ist.berkeley.edu>
2023-12-17 10:09:13 -0800

    rewrote take_qa_test.py to be more modular and to make it easier to support new datasets

--- take_qa_test.py
+++ take_qa_test.py
@@ -4,84 +4,101 @@ from datasets import load_dataset
 import random
 import string
 
-def make_question_string(question_data):
-    num_original_choices = len(question_data['endings'])
-    assert(num_original_choices <= 25) # we only have 26 capital letters
-    choices = [string.ascii_uppercase[i] + '. ' + question_data['endings'][i] for i in range(num_original_choices)] + [string.ascii_uppercase[num_original_choices] + ". I don't know"]
-    return question_data['ctx'] + '\n' + '\n'.join(choices)
+class Test(object):
+    def __init__(self, args):
+        (self.start_q, self.end_q) = (0, 500)
+        self.model = generate_text.Generator(args)
 
-def make_prompt(question_string):
-        return f"""Below is a multiple-choice question. Choose the letter which best answers the question. Keep your response as brief as possible; just state the letter corresponding to your answer, followed by a period, with no explanation."
+        dataset_name = args['dataset'].lower()
+        dataset_name_map = {'hellaswag':'Rowan/hellaswag'}
+        # Different datasets have different keys for the questions and answers
+        q_field_name_map = {'hellaswag':'ctx'}
+        a_field_name_map = {'hellaswag':'label'}
+        choices_field_name_map = {'hellaswag':'endings'}
+        if dataset_name not in dataset_name_map:
+            raise Exception("Unsupported dataset name")
+        self.q_field_name = q_field_name_map[dataset_name]
+        self.a_field_name = a_field_name_map[dataset_name]
+        self.choices_field_name = choices_field_name_map[dataset_name]
+        self.args = args
+        self.questions = load_dataset(dataset_name_map[dataset_name], split='train')
 
-### Question:
-{question_string}
+    def write_output(self, correct, incorrect, abstained):
+        halu_str = '_halu_thresh_' + str(self.args['threshold']) if self.args['check_for_halu'] else ''
+        dataset_str = self.args['dataset'].split("/")[-1]
+        output_filename = "results/%s%s-%s-q%dto%d.txt" % (self.args['model'], halu_str, dataset_str, start_q, end_q)
+        with open(output_filename, 'w') as f:
+            f.write("model = " + self.args['model'] + halu_str + '\n')
+            f.write("Correct: %d | Wrong: %d | Abstained: %d\n" % (len(correct), len(incorrect), len(abstained)))
+            f.write("Score (even): %d\n" % (len(correct) - len(incorrect)))
+            f.write("Score (harsher): %d\n" % (len(correct) - 2 * len(incorrect)))
 
-### Response:\n
-"""
+    def make_question_string(self, choices, question):
+        assert(len(choices) <= 25) # we only have 26 capital letters and need 1 for uncertain
+        choices_with_uncertain = [string.ascii_uppercase[i] + '. ' + choice for (i, choice) in enumerate(choices)] + [string.ascii_uppercase[len(choices)] + ". I don't know"]
+        return question + '\n' + '\n'.join(choices_with_uncertain)
 
-def grade_answers(question_data, llm_output):
-    # Find first instance of A./B./C. etc, if any
-    targets = [c + '.' for c in string.ascii_uppercase][:len(question_data['endings']) + 1] # +1 corresponds to the "I don't know" answer we added
-    target_idxs = [llm_output.find(t) for t in targets if llm_output.find(t) != -1]
-    if len(target_idxs) > 0:
-        llm_answer = llm_output[min(target_idxs)]
-        correct_answer = string.ascii_uppercase[int(question_data['label'])]
-        uncertain_answer = string.ascii_uppercase[len(question_data['endings'])]
-        if llm_answer == uncertain_answer:
-            return f"{llm_answer} (uncertain)"
-        elif llm_answer == correct_answer:
-            return f"{llm_answer}. (correct)"
-        else:
-            return f"{llm_answer}. (incorrect {correct_answer}.)"
-    else:
-        return f"Could not parse answer. (incorrect {correct_answer}.)"
+    def make_prompt(self, question_string):
+            return f"""Below is a multiple-choice question. Choose the letter which best answers the question. Keep your response as brief as possible; just state the letter corresponding to your answer, followed by a period, with no explanation."
 
-def run_test(model, questions, start_q, end_q):
-    correct = []
-    incorrect = []
-    abstained = []
-    for i, question_data in enumerate(questions):
-        if start_q <= i < end_q:
-            question_string = make_question_string(question_data)
-            prompt = make_prompt(question_string)
-            # The brackets and 0 indices are because the inputs/outputs in Generator are lists, for batching. For example, if you set num_responses > 1. For Q&A testing, we only take the first response.
-            formatted_prompt = model.prepare_for_chat([prompt])[0]
+    ### Question:
+    {question_string}
 
-            print(f"Question {i+1}: {question_string}")
-            letter_for_uncertain = string.ascii_uppercase[len(question_data['endings'])]
-            llm_output = model.generate([prompt], letter_for_uncertain)[0]
-            print(f"LLM output: {llm_output}")
-            answer_output = grade_answers(question_data, llm_output)
-            print(f"LLM answer: {answer_output}\n")
+    ### Response:\n
+    """
 
-            if "(correct)" in answer_output:
-                correct.append((i+1, question_string, answer_output))
-            elif "(incorrect" in answer_output:
-                incorrect.append((i+1, question_string, answer_output))
+    def grade_answers(self, choices, correct_answer_idx, llm_output):
+        # Find first instance of A./B./C. etc, if any
+        targets = [c + '.' for c in string.ascii_uppercase][:len(choices) + 1] # +1 corresponds to the "I don't know" answer we added
+        target_idxs = [llm_output.find(t) for t in targets if llm_output.find(t) != -1]
+        if len(target_idxs) > 0:
+            llm_answer = llm_output[min(target_idxs)]
+            correct_answer = string.ascii_uppercase[correct_answer_idx]
+            uncertain_answer = string.ascii_uppercase[len(choices)]
+            if llm_answer == uncertain_answer:
+                return f"{llm_answer} (uncertain)"
+            elif llm_answer == correct_answer:
+                return f"{llm_answer}. (correct)"
             else:
-                abstained.append((i+1, question_string, answer_output))
-            print("Correct: %d | Wrong: %d | Abstained: %d\n" % (len(correct), len(incorrect), len(abstained)))
-    return (correct, incorrect, abstained)
+                return f"{llm_answer}. (incorrect {correct_answer}.)"
+        else:
+            return f"Could not parse answer. (incorrect {correct_answer}.)"
 
-def load_questions(dataset_name):
-    name_map = {'hellaswag':'Rowan/hellaswag'}
-    return load_dataset(name_map[dataset_name.lower()], split='train')
+    def run_test(self):
+        correct = []
+        incorrect = []
+        abstained = []
+        for (i, question_data) in enumerate(self.questions):
+            if self.start_q <= i < self.end_q:
+                choices = question_data[self.choices_field_name]
+                question = question_data[self.q_field_name]
+                correct_answer_idx = int(question_data[self.a_field_name])
+                question_string = self.make_question_string(choices, question)
+                prompt = self.make_prompt(question_string)
+                # The brackets and 0 indices are because the inputs/outputs in Generator are lists, for batching. For example, if you set num_responses > 1. For Q&A testing, we only take the first response.
+                formatted_prompt = self.model.prepare_for_chat([prompt])[0]
+
+                print(f"Question {i+1}: {question_string}")
+                letter_for_uncertain = string.ascii_uppercase[len(choices)]
+                llm_output = self.model.generate([prompt], letter_for_uncertain)[0]
+                print(f"LLM output: {llm_output}")
+                answer_output = self.grade_answers(choices, correct_answer_idx, llm_output)
+                print(f"LLM answer: {answer_output}\n")
+
+                if "(correct)" in answer_output:
+                    correct.append((i+1, question_string, answer_output))
+                elif "(incorrect" in answer_output:
+                    incorrect.append((i+1, question_string, answer_output))
+                else:
+                    abstained.append((i+1, question_string, answer_output))
+                print("Correct: %d | Wrong: %d | Abstained: %d\n" % (len(correct), len(incorrect), len(abstained)))
+        return (correct, incorrect, abstained)
 
 def main():
     args = generate_text.parse_args()
-    (start_q, end_q) = (0, 500)
-    questions = load_questions(args['dataset'])
-    model = generate_text.Generator(args)
-    (correct, incorrect, abstained) = run_test(model, questions, start_q, end_q)
-    halu_str = '_halu_check_' + str(args['threshold']) if args['check_for_halu'] else ''
-    dataset_str = args['dataset'].split("/")[-1]
-    output_filename = "results/%s%s-%s-q%dto%d.txt" % (args['model'], halu_str, dataset_str, start_q, end_q)
-    with open(output_filename, 'w') as f:
-        f.write("model = " + args['model'] + halu_str + '\n')
-        f.write("Correct: %d | Wrong: %d | Abstained: %d\n" % (len(correct), len(incorrect), len(abstained)))
-        f.write("Score (even grading): %d\n" % (len(correct) - len(incorrect)))
-        f.write("Score (harsher grading): %d\n" % (len(correct) - 2 * len(incorrect)))
-        f.write("Score (very harsh): %d" % (len(correct) - 4 * len(incorrect)))
+    test = Test(args)
+    (correct, incorrect, abstained) = test.run_test()
+    test.write_output(correct, incorrect, abstained)
 
 if __name__ == '__main__':
     main()
