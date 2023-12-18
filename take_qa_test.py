6f035de321d424d155dfd0a7d15dae9d6746316a
Ben Plaut <bplaut@rnn.ist.berkeley.edu>
2023-12-18 07:44:01 -0800

    modified prompt to not have #, plus some minor formatting

--- take_qa_test.py
+++ take_qa_test.py
@@ -2,33 +2,32 @@ import generate_text
 import json
 from datasets import load_dataset
 import random
-import string
-
+from string import ascii_uppercase
 class Test(object):
     def __init__(self, args):
-        (self.start_q, self.end_q) = (0, 1000)
+        (self.start_q, self.end_q) = (0, 500)
         self.model = generate_text.Generator(args)
 
-        dataset_name = args['dataset'].lower()
-        dataset_args = {'hellaswag':('Rowan/hellaswag',),
+        dset_name = args['dataset'].lower()
+        dset_args = {'hellaswag':('Rowan/hellaswag',),
                         'arc-easy':('ai2_arc', 'ARC-Easy'),
                         'arc-challenge':('ai2_arc', 'ARC-Challenge'),
         }
         # Different datasets have different keys for the questions and answers
-        self.get_q = lambda x: (x['ctx'] if dataset_name == 'hellaswag' else
-                                x['question'] if dataset_name == 'ai2_arc' else
+        self.get_q = lambda x: (x['ctx'] if dset_name == 'hellaswag' else
+                                x['question'] if dset_name == 'ai2_arc' else
                                 None)
-        self.get_a = lambda x: (string.ascii_uppercase[int(x['label'])] if dataset_name == 'hellaswag' else
-                                x['answerKey'] if dataset_name == 'ai2_arc' else
+        self.get_a = lambda x: (ascii_uppercase[int(x['label'])] if dset_name=='hellaswag' else
+                                x['answerKey'] if dset_name == 'ai2_arc' else
                                 None)
-        self.get_choices = lambda x: (x['endings'] if dataset_name == 'hellaswag' else
-                                      x['choices']['text'] if dataset_name == 'ai2_arc' else
+        self.get_choices = lambda x: (x['endings'] if dset_name == 'hellaswag' else
+                                      x['choices']['text'] if dset_name == 'ai2_arc' else
                                       None)
 
-        if dataset_name not in dataset_args:
+        if dset_name not in dset_args:
             raise Exception("Unsupported dataset name")
         self.args = args
-        self.questions = load_dataset(*dataset_args[dataset_name], split='train')
+        self.questions = load_dataset(*dset_args[dset_name], split='train')
 
     def write_output(self, correct, incorrect, abstained):
         halu_str = '_halu_thresh_' + str(self.args['threshold']) if self.args['check_for_halu'] else ''
@@ -43,25 +42,25 @@ class Test(object):
 
     def make_question_string(self, choices, question):
         assert(len(choices) <= 25) # we only have 26 capital letters and need 1 for uncertain
-        choices_with_uncertain = [string.ascii_uppercase[i] + '. ' + choice for (i, choice) in enumerate(choices)] + [string.ascii_uppercase[len(choices)] + ". I don't know"]
+        choices_with_uncertain = [ascii_uppercase[i] + '. ' + choice for (i, choice) in enumerate(choices)] + [ascii_uppercase[len(choices)] + ". I don't know"]
         return question + '\n' + '\n'.join(choices_with_uncertain)
 
     def make_prompt(self, question_string):
             return f"""Below is a multiple-choice question. Choose the letter which best answers the question. Keep your response as brief as possible; just state the letter corresponding to your answer, followed by a period, with no explanation."
 
-### Question:
+Question:
 {question_string}
 
-### Response:\n
+Response:\n
 """
 
     def grade_answers(self, choices, correct_answer, llm_output):
         # Find first instance of A./B./C. etc, if any
-        targets = [c + '.' for c in string.ascii_uppercase][:len(choices) + 1] # +1 corresponds to the "I don't know" answer we added
+        targets = [c + '.' for c in ascii_uppercase][:len(choices) + 1] # +1 corresponds to the "I don't know" answer we added
         target_idxs = [llm_output.find(t) for t in targets if llm_output.find(t) != -1]
         if len(target_idxs) > 0:
             llm_answer = llm_output[min(target_idxs)]
-            uncertain_answer = string.ascii_uppercase[len(choices)]
+            uncertain_answer = ascii_uppercase[len(choices)]
             if llm_answer == uncertain_answer:
                 return f"{llm_answer} (uncertain)"
             elif llm_answer == correct_answer:
@@ -75,9 +74,10 @@ class Test(object):
         correct = []
         incorrect = []
         abstained = []
+
+        prompts = []
         for (i, question_data) in enumerate(self.questions):
             if self.start_q <= i < self.end_q:
-                print('question data:', question_data)
                 choices = self.get_choices(question_data)
                 question = self.get_q(question_data)
                 correct_answer = self.get_a(question_data)
@@ -87,7 +87,7 @@ class Test(object):
                 formatted_prompt = self.model.prepare_for_chat([prompt])[0]
 
                 print(f"Question {i+1}: {question_string}")
-                letter_for_uncertain = string.ascii_uppercase[len(choices)]
+                letter_for_uncertain = ascii_uppercase[len(choices)]
                 llm_output = self.model.generate([prompt], letter_for_uncertain)[0]
                 print(f"LLM output: {llm_output}")
                 answer_output = self.grade_answers(choices, correct_answer, llm_output)
