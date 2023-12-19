94fa33b227a6cdd95b3ce22f69382d1fb28d948d
Ben Plaut <bplaut@rnn.ist.berkeley.edu>
2023-12-19 14:13:41 -0800

    removed threshold parameter from run_qa_tests.sh, since we can do them all at once now

--- run_qa_tests.sh
+++ run_qa_tests.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 
 # Check if the correct number of arguments are provided
-if [ "$#" -ne 4 ]; then
+if [ "$#" -ne 3 ]; then
     echo "Error: Incorrect number of arguments."
-    echo "Usage: ./run_qa_tests.sh 'model1,model2' 'threshold1,threshold2' 'dataset1,dataset2' question_range"
+    echo "Usage: ./run_qa_tests.sh 'model1,model2' 'dataset1,dataset2' question_range"
     exit 1
 fi
 
 # Read the argument options from the command line input and split them into arrays
 IFS=',' read -r -a model_options <<< "$1"
-IFS=',' read -r -a threshold_options <<< "$2"
-IFS=',' read -r -a dataset_options <<< "$3"
-question_range="$4"
+IFS=',' read -r -a dataset_options <<< "$2"
+question_range="$3"
 
 # Function to determine batch_size based on model name
 get_batch_size() {
@@ -33,21 +32,18 @@ get_batch_size() {
     echo "$batch_size"
 }
 
-# Loop through each combination of arguments (excluding question_range)
+# Loop through each combination of model and dataset (excluding question_range)
 for model in "${model_options[@]}"
 do
-    for threshold in "${threshold_options[@]}"
+    for dataset in "${dataset_options[@]}"
     do
-        for dataset in "${dataset_options[@]}"
-        do
-            # Determine batch_size based on the model
-            batch_size=$(get_batch_size "$model")
+        # Determine batch_size based on the model
+        batch_size=$(get_batch_size "$model")
 
-            # Running the command with the arguments
-            echo -e "\nRunning take_qa_test.py with arguments: --model=$model --threshold=$threshold --dataset=$dataset --question_range=$question_range --batch_size=$batch_size"
-            python take_qa_test.py --model="$model" --threshold="$threshold" --dataset="$dataset" --question_range="$question_range" --batch_size="$batch_size" --max_new_tokens=100
-        done
+        # Running the command with the arguments
+        echo -e "\nRunning take_qa_test.py with arguments: --model=$model --dataset=$dataset --question_range=$question_range --batch_size=$batch_size"
+        python take_qa_test.py --model="$model" --dataset="$dataset" --question_range="$question_range" --batch_size="$batch_size" --max_new_tokens=100
     done
 done
 
-echo "Script run_qa_tests.sh completed."
+echo "Script completed."
