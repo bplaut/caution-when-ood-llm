27a0fc702b7589e8c4fd59a891d59df9eefe9ed3
Ben Plaut <bplaut@rnn.ist.berkeley.edu>
2023-12-24 09:04:41 -0800

    improvements to make_latex_table.py, now creates one table for each dataset

--- make_latex_table.py
+++ make_latex_table.py
@@ -1,8 +1,10 @@
 import sys
+from collections import defaultdict
 
 def parse_and_convert_to_tex(input_file_path, output_file_path):
     """
-    Parses the input txt file and converts the data into a .tex file format.
+    Parses the input txt file and converts the data into a .tex file format,
+    creating separate tables for each group.
 
     Args:
     input_file_path (str): Path to the input txt file.
@@ -13,29 +15,35 @@ def parse_and_convert_to_tex(input_file_path, output_file_path):
         lines = f.readlines()
 
     # Process the data and prepare for LaTeX format
-    data = []
+    data = defaultdict(list)
     section_length = 5 # name, correct, wrong, abstained, newline
     for i in range(0, len(lines), section_length):
-        name = lines[i].split('.txt')[0].replace('_', ' ')
+        full_name = lines[i].split('.txt')[0]
+        group_name, name = full_name.split('_', 1)
+        name = name.replace('_',' ')
         correct = int(lines[i+1].split(':')[1].strip())
         wrong = int(lines[i+2].split(':')[1].strip())
         abstained = int(lines[i+3].split(':')[1].strip())
         score_1 = correct - wrong
         score_2 = correct - 2 * wrong
-        data.append([name, correct, wrong, abstained, score_1, score_2])
+        data[group_name].append([name, correct, wrong, abstained, score_1, score_2])
 
     # Convert the data to LaTeX table format
     with open(output_file_path, 'w') as f:
         f.write('\\documentclass{article}\n')
+        f.write('\\renewcommand\\arraystretch{1.4}\n')
         f.write('\\begin{document}\n')
-        f.write('\\begin{tabular}{c|c|c|c|c|c|}\n')
-        f.write('\\hline\n')
-        f.write('Name & Correct & Wrong & Abstained & Score 1 & Score 2 \\\\\n')
-        f.write('\\hline\n')
-        for row in data:
-            f.write(f'{row[0]} & {row[1]} & {row[2]} & {row[3]} & {row[4]} & {row[5]} \\\\\n')
+        for group_name, rows in data.items():
+            f.write(f'\\section*{{{group_name}}}\n')
+            f.write('\\begin{tabular}{c|c|c|c|c|c|}\n')
             f.write('\\hline\n')
-        f.write('\\end{tabular}\n')
+            f.write('Name & Correct & Wrong & Abstained & Score 1 & Score 2 \\\\\n')
+            f.write('\\hline\n')
+            for row in rows:
+                f.write(f'{row[0]} & {row[1]} & {row[2]} & {row[3]} & {row[4]} & {row[5]} \\\\\n')
+                f.write('\\hline\n')
+            f.write('\\end{tabular}\n')
+            f.write('\\newpage\n')  # Optional: Start each group table on a new page
         f.write('\\end{document}')
     print(f"Successfully wrote to {output_file_path}")
 
