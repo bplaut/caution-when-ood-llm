a07867c7a661a2be759722b63b6db367d855ea84
Ben Plaut <bplaut@rnn.ist.berkeley.edu>
2023-12-25 12:06:39 -0800

    updated run_qa_tests.sh to take multiple question ranges, so I don't have to run separate commands for 0-2000, 2000-4000, etc

--- run_qa_tests.sh
+++ run_qa_tests.sh
@@ -3,15 +3,14 @@
 # Check if the correct number of arguments are provided
 if [ "$#" -ne 3 ]; then
     echo "Error: Incorrect number of arguments."
-    echo "Usage: ./run_qa_tests.sh 'model1,model2' 'dataset1,dataset2' question_range"
+    echo "Usage: ./run_qa_tests.sh 'model1,model2' 'dataset1,dataset2' 'question_range1,question_range2'"
     exit 1
 fi
 
 # Read the argument options from the command line input and split them into arrays
 IFS=',' read -r -a model_options <<< "$1"
 IFS=',' read -r -a dataset_options <<< "$2"
-question_range="$3"
-
+IFS=',' read -r -a question_ranges <<< "$3"
 
 # Function to determine batch_size based on model and dataset names
 get_batch_size() {
@@ -45,18 +44,21 @@ get_batch_size() {
     echo "$batch_size"
 }
 
-# Loop through each combination of model and dataset (excluding question_range)
-for model in "${model_options[@]}"
+# Loop through each combination of model, dataset, and question_range
+for question_range in "${question_ranges[@]}"
 do
     for dataset in "${dataset_options[@]}"
     do
-        # Determine batch_size based on the model and dataset
-        batch_size=$(get_batch_size "$model" "$dataset")
-	log_file="logs/${model}_${dataset}_${question_range}_log.txt"
+	for model in "${model_options[@]}"
+        do
+            # Determine batch_size based on the model and dataset
+            batch_size=$(get_batch_size "$model" "$dataset")
+            log_file="logs/${model}_${dataset}_${question_range}_log.txt"
 
-        # Running the command with the arguments
-        echo -e "\nRunning take_qa_test.py with arguments: --model=$model --dataset=$dataset --question_range=$question_range --batch_size=$batch_size"
-        python take_qa_test.py --model="$model" --dataset="$dataset" --question_range="$question_range" --batch_size="$batch_size" --max_new_tokens=100 &> "$log_file"
+            # Running the command with the arguments
+            echo -e "\nRunning take_qa_test.py with arguments: --model=$model --dataset=$dataset --question_range=$question_range --batch_size=$batch_size"
+            python take_qa_test.py --model="$model" --dataset="$dataset" --question_range="$question_range" --batch_size="$batch_size" --max_new_tokens=100 &> "$log_file"
+        done
     done
 done
 
