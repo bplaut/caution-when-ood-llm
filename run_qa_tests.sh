56c86aaf1deb7d0fb8094a098bc3b65b9683ecf9
Ben Plaut <bplaut@rnn.ist.berkeley.edu>
2023-12-29 12:22:50 -0800

    run_qa_tests.sh supports abstain_option command line arg now

--- run_qa_tests.sh
+++ run_qa_tests.sh
@@ -3,7 +3,7 @@
 # Check if the correct number of arguments are provided
 if [ "$#" -lt 3 ] || [ "$#" -gt 4 ]; then
     echo "Error: Incorrect number of arguments."
-    echo "Usage: ./run_qa_tests.sh 'model1,model2' 'dataset1,dataset2' 'question_range1,question_range2' ['two_choices']"
+    echo "Usage: ./run_qa_tests.sh 'model1,model2' 'dataset1,dataset2' 'question_range1,question_range2' ['abstain_option']"
     exit 1
 fi
 
@@ -11,7 +11,7 @@ fi
 IFS=',' read -r -a model_options <<< "$1"
 IFS=',' read -r -a dataset_options <<< "$2"
 IFS=',' read -r -a question_ranges <<< "$3"
-two_choices=${4:-}
+abstain_option=${4:-}
 
 # Function to determine batch_size based on model and dataset names
 # Function to determine batch_size based on model and dataset names
@@ -30,11 +30,11 @@ get_batch_size() {
         "Llama-13b")
             batch_size=22
             ;;
-        "Mistral"|"Zephyr"|"Sakura-Solar")
-            batch_size=160
+        "Mistral"|"Zephyr")
+            batch_size=168
             ;;
         *)
-            batch_size=60 # Default value
+            batch_size=128 # Default value
             ;;
     esac
 
@@ -59,19 +59,20 @@ do
             # Determine batch_size based on the model and dataset
             batch_size=$(get_batch_size "$model" "$dataset")
 
-            # Modify log file name based on the presence of two_choices
-            if [ -z "$two_choices" ]; then
+            # Modify log file name based on the presence of abstain_option
+            if [ -z "$abstain_option" ]; then
                 log_file="logs/${model}_${dataset}_${question_range}_log.txt"
             else
-                log_file="logs/${model}_${dataset}_${question_range}_${two_choices}_log.txt"
+                log_file="logs/${model}_${dataset}_${question_range}_${abstain_option}_log.txt"
             fi
 	    
             # Running the command with the arguments
-            echo -e "\nRunning take_qa_test.py with arguments: --model=$model --dataset=$dataset --question_range=$question_range --batch_size=$batch_size --max_new_tokens=100 $two_choices"
-            if [ -z "$two_choices" ]; then
+            if [ -z "$abstain_option" ]; then
+		echo -e "\nRunning take_qa_test.py with arguments: --model=$model --dataset=$dataset --question_range=$question_range --batch_size=$batch_size --max_new_tokens=100"
                 python take_qa_test.py --model="$model" --dataset="$dataset" --question_range="$question_range" --batch_size="$batch_size" --max_new_tokens=100 &> "$log_file"
             else
-                python take_qa_test.py --model="$model" --dataset="$dataset" --question_range="$question_range" --batch_size="$batch_size" --max_new_tokens=100 --two_choices &> "$log_file"
+		echo -e "\nRunning take_qa_test.py with arguments: --model=$model --dataset=$dataset --question_range=$question_range --batch_size=$batch_size --max_new_tokens=100 --abstain_option"
+                python take_qa_test.py --model="$model" --dataset="$dataset" --question_range="$question_range" --batch_size="$batch_size" --max_new_tokens=100 --abstain_option &> "$log_file"
             fi	    
         done
     done
