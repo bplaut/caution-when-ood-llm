afb9226177539ad528370cfb87f08e53c18983e6
Ben Plaut <bplaut@rnn.ist.berkeley.edu>
2024-01-03 17:08:52 -0800

    updating post-processing script to handle new naming convention

--- do_post_processing.sh
+++ do_post_processing.sh
@@ -10,7 +10,7 @@ fi
 thresholds1=$1
 thresholds2=$2
 
-python combine_grades_into_table.py tables/no_abstain.tex $thresholds1 True results/*no_abstain.txt
-python plot_data.py figs/no_abstain True 0-1 results/*no_abstain.txt
+python combine_grades_into_table.py tables/no_abstain_normed_logits.tex $thresholds1 True results/*no_abstain.txt results/*no_abstain_normed_logits.txt
+python plot_data.py figs/no_abstain_normed_logits True 0-1 results/*no_abstain.txt results/*no_abstain_normed_logits.txt
 python combine_grades_into_table.py tables/no_abstain_raw_logits.tex $thresholds2 True results/*no_abstain_raw_logits.txt
 python plot_data.py figs/no_abstain_raw_logits True 0-1 results/*no_abstain_raw_logits.txt
