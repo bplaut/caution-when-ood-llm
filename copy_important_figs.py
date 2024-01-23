ab322f83ea736e2dd60f91e29bce83304b8cceaa
Ben Plaut <bplaut@rnn.ist.berkeley.edu>
2024-01-23 07:58:04 -0800

    copy_important_figs.py now supports pdf and png

--- copy_important_figs.py
+++ copy_important_figs.py
@@ -8,18 +8,18 @@ def copy_files(output_directory: str, filepaths: List[str]):
         os.makedirs(output_directory)
 
     for filepath in filepaths:
-        filepath = filepath + '.pdf'
         print("Copying ", filepath)
-        if os.path.isfile(filepath):
-            # Copy file to output directory
-            base_name = os.path.basename(filepath)[:-4]
-            dir_name = os.path.dirname(filepath)
-            prompt_str = '' if 'prompt' in base_name else '_first_prompt' if 'first_prompt' in dir_name else '_second_prompt' if 'second_prompt' in dir_name else ''
-            logit_str = '' if ('logit' in base_name.lower() or 'MSP' in base_name) else '_norm_logits' if 'norm_logits' in dir_name else '_raw_logits' if 'raw_logits' in dir_name else ''
-            new_path = os.path.join(output_directory, base_name + prompt_str + logit_str + '.pdf')
-            shutil.copy(filepath, new_path)
-        else:
-            print(f"File not found: {filepath}")
+        extensions = ['.pdf', '.png']
+        for extension in extensions:
+            full_path = filepath + extension
+            if os.path.isfile(full_path):
+                # Copy file to output directory
+                base_name = os.path.basename(full_path)[:-4]
+                dir_name = os.path.dirname(full_path)
+                prompt_str = '' if 'prompt' in base_name else '_first_prompt' if 'first_prompt' in dir_name else '_second_prompt' if 'second_prompt' in dir_name else ''
+                logit_str = '' if ('logit' in base_name.lower() or 'MSP' in base_name) else '_norm_logits' if 'norm_logits' in dir_name else '_raw_logits' if 'raw_logits' in dir_name else ''
+                new_path = os.path.join(output_directory, base_name + prompt_str + logit_str + extension)
+                shutil.copy(full_path, new_path)
 
 output_dir = 'paper_figs'
 cross_group_dir = 'figs/main_figs/cross_group_plots'
