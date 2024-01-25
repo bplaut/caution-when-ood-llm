9426507a523f0f069b578e4543f2eb511723c8f2
Ben Plaut <bplaut@rnn.ist.berkeley.edu>
2024-01-25 13:48:03 -0800

    made collapse_prompts and incl_unparseable command line args of do_post_processing.sh

--- do_post_processing.sh
+++ do_post_processing.sh
@@ -1,45 +1,19 @@
 #!/bin/bash
 
-# Check if no more than three arguments are provided
-if [ $# -gt 3 ]; then
-    echo "Incorrect number of arguments provided. Usage: ./do_post_processing <directory> [comma-separated list of probability thresholds] [comma-separated list of raw logit thresholds]"
+# Check if exactly three arguments are provided
+if [ $# -ne 3 ]; then
+    echo "Incorrect number of arguments provided. Usage: ./do_post_processing <directory> <collapse_prompts> <incl_unparseable>"
     exit 1
 fi
 
-# Check if at least one argument is provided (the directory)
-if [ $# -lt 1 ]; then
-    echo "No directory specified. Usage: ./do_post_processing <directory> [comma-separated list of probability thresholds] [comma-separated list of raw logit thresholds]"
-    exit 1
-fi
-
-# Assign the arguments to variables. If no values are provided for thresholds, default to 0
+# Assign the arguments to variables
 dir=$1
-thresholds1=${2:-0}
-thresholds2=${3:-0}
+collapse_prompts=$2
+incl_unparseable=$3
 
-echo -e "Making tables...\n"
-output_dir=tables
+echo -e "collapse_prompts: $collapse_prompts, incl_unparseable: $incl_unparseable\n"
 
-for abstain in "no_abst" "yes_abst"; do
-    for logit_type in "norm" "raw"; do
-        for prompt in "first_prompt" "second_prompt"; do
-            # Construct the file name part for table command
-            filename_part="${abstain}_${logit_type}_logits_${prompt}"
-            # Construct the threshold variable
-            if [ "$logit_type" = "norm" ]; then
-                thresholds=$thresholds1
-            else
-                thresholds=$thresholds2
-            fi
-            # Run the table command
-            python combine_grades_into_table.py $output_dir/${filename_part}.tex $thresholds True $dir/*${filename_part}.txt
-        done
-    done
-done
-
-output_dir=figs
-incl_unparseable=True
-collapse_prompts=True
+output_dir="figs_${collapse_prompts}_${incl_unparseable}"
 echo -e "\nMaking figures...\n"
 
 for abstain in "no_abst" "yes_abst"; do
