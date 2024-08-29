6766717bb4f905d0ed7401ecf0e62fd939b41adb
Ben Plaut <bplaut@rnn.ist.berkeley.edu>
2024-08-28 17:01:38 -0700

    added few_shot option to run_qa_tests.sh, also removed abstain option and piqa case

--- run_qa_tests.sh
+++ run_qa_tests.sh
@@ -3,8 +3,8 @@
 # Check if the correct number of arguments are provided
 if [ "$#" -ne 5 ]; then
     echo "Error: Incorrect number of arguments."
-    echo "Usage format: ./run_qa_tests.sh model1,model2 dataset1,dataset2,dataset3 question_range1,question_range2 prompt_phrasing abstain_option"
-    echo "Example usage: ./run_qa_tests.sh Mistral,Llama-13b,Llama-70b arc,truthfulqa 0-500,500-1000 0 False"
+    echo "Usage format: ./run_qa_tests.sh model1,model2 dataset1,dataset2,dataset3 question_range1,question_range2 prompt_phrasing few_shot"
+    echo "Example usage: ./run_qa_tests.sh Mistral,Llama-13b,Llama-70b arc,truthfulqa 0-500,500-1000 0 True"
     exit 1
 fi
 
@@ -13,7 +13,7 @@ IFS=',' read -r -a model_options <<< "$1"
 IFS=',' read -r -a dataset_options <<< "$2"
 IFS=',' read -r -a question_ranges <<< "$3"
 prompt_phrasing="$4"
-abstain_option="$5"
+few_shot="$5"
 
 # Function to determine batch_size based on model and dataset names
 get_batch_size() {
@@ -58,7 +58,39 @@ get_batch_size() {
     if [ "$dataset_name" = "mmlu" ]; then
         batch_size=$(( (batch_size + 2) / 3 ))
     fi
+
     echo "$batch_size"
+}
+
+# Function to determine few_shot_number based on dataset and few_shot flag
+get_few_shot_number() {
+    local dataset_name="$1"
+    local few_shot_flag="$2"
+
+    if [ "$few_shot_flag" = "False" ]; then
+        echo 0
+    else
+        case "$dataset_name" in
+            "arc")
+                echo 25
+                ;;
+            "hellaswag")
+                echo 10
+                ;;
+            "mmlu")
+                echo 5
+                ;;
+            "truthfulqa")
+                echo 6
+                ;;
+            "winogrande")
+                echo 5
+                ;;
+            *)
+                echo 0
+                ;;
+        esac
+    fi
 }
 
 # Create logs directory if it doesn't exist
@@ -74,12 +106,22 @@ do
             # Determine batch_size based on the model and dataset
             batch_size=$(get_batch_size "$model" "$dataset")
 
+            # Determine few_shot_number based on dataset and few_shot flag
+            few_shot_number=$(get_few_shot_number "$dataset" "$few_shot")
+
             # Define log file name
-            log_file="logs/${model}_${dataset}_${question_range}_abstain-option-${abstain_option}_prompt-phrasing-${prompt_phrasing}_log.txt"
+            log_file="logs/${model}_${dataset}_${question_range}_prompt-phrasing-${prompt_phrasing}"
+
+            # Append "few_shot" to log file name if few_shot is True
+            if [ "$few_shot" = "True" ]; then
+                log_file="${log_file}_few_shot"
+            fi
+
+            log_file="${log_file}_log.txt"
 
             # Running the command with the arguments
-            echo -e "\nRunning take_qa_test.py with arguments: --model=$model --dataset=$dataset --question_range=$question_range --batch_size=$batch_size --abstain_option=$abstain_option --prompt_phrasing=$prompt_phrasing"
-            python take_qa_test.py --model="$model" --dataset="$dataset" --question_range="$question_range" --batch_size="$batch_size" --abstain_option="$abstain_option" --prompt_phrasing="$prompt_phrasing" --max_new_tokens=100 --num_top_tokens=1 &> "$log_file"
+            echo -e "\nRunning take_qa_test.py with arguments: --model=$model --dataset=$dataset --question_range=$question_range --batch_size=$batch_size --prompt_phrasing=$prompt_phrasing --few_shot_number=$few_shot_number"
+            python take_qa_test.py --model="$model" --dataset="$dataset" --question_range="$question_range" --batch_size="$batch_size" --prompt_phrasing="$prompt_phrasing" --few_shot_number="$few_shot_number" --max_new_tokens=100 --num_top_tokens=1 &> "$log_file"
         done
     done
 done
