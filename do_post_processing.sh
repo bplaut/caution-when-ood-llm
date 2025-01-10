7fe128f8e11ba551fb08f94b70ad97102beadda0
Ben Plaut <bplaut@rnn.ist.berkeley.edu>
2025-01-09 18:02:24 -0800

    changed do_post_processing.sh to also take an output_dir. Now the script automatically puts the important figs in that output dir

--- do_post_processing.sh
+++ do_post_processing.sh
@@ -1,36 +1,37 @@
 #!/bin/bash
 
 # Check if exactly one argument is provided
-if [ $# -ne 1 ]; then
-    echo "Incorrect number of arguments provided. Usage: ./do_post_processing <directory>"
+if [ $# -ne 2 ]; then
+    echo "Incorrect number of arguments provided. Usage: ./do_post_processing <input_directory> <output_directory>"
     exit 1
 fi
 
 # Assign the argument to a variable
-dir=$1
+input_dir=$1
+final_output_dir=$2
 
 # Loop over the two values of collapse_prompts
 for collapse_prompts in True False
 do
     # Construct the output directory name
-    output_dir="all_figs_${dir}/collapse_${collapse_prompts}"
+    all_figs_output_dir="all_figs_${input_dir}/collapse_${collapse_prompts}"
     echo -e "\nMAKING FIGURES FOR COLLAPSE_PROMPTS=${collapse_prompts}\n"
 
-    python plot_data.py "$output_dir/main_figs" arc,hellaswag,mmlu,truthfulqa,winogrande "$collapse_prompts" "$dir"/*.txt
-    python plot_data.py "$output_dir/arc" arc "$collapse_prompts" "$dir"/*.txt
-    python plot_data.py "$output_dir/hellaswag" hellaswag "$collapse_prompts" "$dir"/*.txt
-    python plot_data.py "$output_dir/mmlu" mmlu "$collapse_prompts" "$dir"/*.txt
-    python plot_data.py "$output_dir/truthfulqa" truthfulqa "$collapse_prompts" "$dir"/*.txt
-    python plot_data.py "$output_dir/winogrande" winogrande "$collapse_prompts" "$dir"/*.txt
+    python plot_data.py "$all_figs_output_dir/main_figs" arc,hellaswag,mmlu,truthfulqa,winogrande "$collapse_prompts" "$input_dir"/*.txt
+    python plot_data.py "$all_figs_output_dir/arc" arc "$collapse_prompts" "$input_dir"/*.txt
+    python plot_data.py "$all_figs_output_dir/hellaswag" hellaswag "$collapse_prompts" "$input_dir"/*.txt
+    python plot_data.py "$all_figs_output_dir/mmlu" mmlu "$collapse_prompts" "$input_dir"/*.txt
+    python plot_data.py "$all_figs_output_dir/truthfulqa" truthfulqa "$collapse_prompts" "$input_dir"/*.txt
+    python plot_data.py "$all_figs_output_dir/winogrande" winogrande "$collapse_prompts" "$input_dir"/*.txt
 
     echo -e "\nCopying important figures...\n"
-    python copy_important_figs.py "$output_dir" "important_figs_collapse_${collapse_prompts}_${dir}"
+    python copy_important_figs.py $all_figs_output_dir $final_output_dir
 
     # Only run statistical tests for AUROC stuff, which uses collapse_prompts=False
     if [ "$collapse_prompts" = "False" ]; then
         echo -e "\nDoing statistical tests...\n"
         for option in {1..4}; do
-            python statistical_tests.py -o "$option" -d "$dir"
+            python statistical_tests.py -o "$option" -d "$input_dir"
         done
     fi
 done
